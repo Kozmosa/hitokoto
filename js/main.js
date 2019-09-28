0f73c7c060a101e2624f7734d9a39b432e7f46fd
Xuyang <2786627598@qq.com>
2019-09-28 11:24:44 +0800

    fix mobile page

--- js/main.js
+++ js/main.js
@@ -2,7 +2,10 @@ function getJinrishici() {
 	jinrishici.load(function (result) {
 		// 自己的处理逻辑
 		console.log(result);
-		document.getElementById('viewer').innerHTML = result.data.content;
+		window.detailData = result.data.origin
+		const content = result.data.content
+		const viewer0 = document.querySelectorAll('#viewer')[0]
+		viewer0.innerHTML = content
 	});
 }
 
@@ -23,4 +26,20 @@ function hitokotoApi() {
 		.catch(function (err) {
 			console.error(err);
 		})
+}
+
+function displayDetails() {
+	// Get details from global object window
+	const detailData = window.detailData
+	const author = detailData.author
+	const title = detailData.title
+	const dynasty = detailData.dynasty
+	const content = detailData.content.toString()
+
+	// Build modal dialog htmls
+	const html = `<h2 style='font-family: Roboto,Noto,Helvetica,Arial,sans-serif;'>${title}</h2><p style='font-family: Roboto,Noto,Helvetica,Arial,sans-serif;'>作者：(${dynasty}) ${author}</p><p style='font-family: Roboto,Noto,Helvetica,Arial,sans-serif;'>全文内容：<br>${content}</p>`
+
+	mdui.dialog({
+		content: html
+	})
 }
